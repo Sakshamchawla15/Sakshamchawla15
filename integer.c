60bf8ee558bd85b7af52109e8f57ee52427c656e
Sakshamchawla15 <92323357+Sakshamchawla15@users.noreply.github.com>
2022-11-08 11:11:28 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,13 @@
+#include"integer.h"
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("Enter the value of a");
+    scanf("%d",&a);
+    printf("Enter the value of b");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("%d",sum);
+    return 0;
+}
